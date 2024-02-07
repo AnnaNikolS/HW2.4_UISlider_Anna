e59b51360792885aa4ee81921adada1bb468bfe2
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-08 00:28:03 +0300

    Initial Commit

---
+++ HW2.4_UISlider_Anna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.4_UISlider_Anna
+//
+//  Created by Анна on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
