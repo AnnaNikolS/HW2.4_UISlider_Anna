19d2bdf4e952d28f93ace6534e223dd836d2e468
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-08 17:15:23 +0300

    project logic

---
+++ HW2.4_UISlider_Anna/Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Extension.swift
+//  HW2.4_UISlider_Anna
+//
+//  Created by Анна on 08.02.2024.
+//
+
+import UIKit
+
+// MARK: Setting the initial values of the sliders
+extension ViewController {
+    func initialValuesOfTheSliders() {
+        valueOfRedSlider.text = "0"
+        valueOfGreenSlider.text = "0"
+        valueOfBlueSlider.text = "0"
+    }
+}
--- HW2.4_UISlider_Anna/ViewController.swift
+++ HW2.4_UISlider_Anna/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentColor: UIView!
+    
+    @IBOutlet weak var valueOfRedSlider: UILabel!
+    @IBOutlet weak var valueOfGreenSlider: UILabel!
+    @IBOutlet weak var valueOfBlueSlider: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var redValue: CGFloat = 0
+    var greenValue: CGFloat = 0
+    var blueValue: CGFloat = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentColor.layer.cornerRadius = 20
+        initialValuesOfTheSliders()
     }
-
-
+    
+    @IBAction func pressedSlider(_ sender: UISlider) {
+        switch sender {
+        case redSlider:
+            redValue = CGFloat(round(redSlider.value  *  100) / 100)
+            valueOfRedSlider.text = redValue.formatted()
+        case greenSlider:
+            greenValue = CGFloat(round(greenSlider.value  *  100) / 100)
+            valueOfGreenSlider.text = greenValue.formatted()
+        default:
+            blueValue = CGFloat(round(blueSlider.value  *  100) / 100)
+            valueOfBlueSlider.text = blueValue.formatted()
+        }
+        
+        currentColor.backgroundColor = UIColor(
+            cgColor: .init(
+                red: redValue,
+                green: greenValue,
+                blue: blueValue,
+                alpha: 1)
+        )
+    }
 }
-
